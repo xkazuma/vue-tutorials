07137addfb9e25f2674b61ec47c961cf6dd5b1f2
Kazuma Kusu <kkusu@acm.org>
2022-04-20 10:48:12 +0900

    learned watcher

---
+++ docs/js/watched-property.js
@@ -0,0 +1,33 @@
+///// watched property
+// var vm = new Vue({
+// 
+//   el: '#demo',
+//   data: {
+//     firstName: 'Foo',
+//     lastName: 'Bar',
+//     fullName: 'Foo Bar',
+//   },
+//   watch: {
+//     firstName: function(val) {
+//       this.fullName = val + ' ' + this.lastName
+//     },
+//     lastName: function(val) {
+//       this.fullName = this.firstName + ' ' + val
+//     }
+//   }
+// })
+
+///// computed property
+var vm = new Vue({
+
+  el: '#demo',
+  data: {
+    firstName: 'Foo',
+    lastName: 'Bar'
+  },
+  computed: {
+    fullName: function() {
+      return this.firstName + ' ' + this.lastName
+    }
+  }
+})
